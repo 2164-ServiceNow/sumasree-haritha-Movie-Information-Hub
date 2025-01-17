af4a75e5b3e67509565fd3b9be80cd3b943851c8
SumaGuruvu <sumaguruvu@gmail.com>
2025-01-17 22:52:42 +0530

    added search functionality

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
-'use strict';
+"use strict";
+let app = angular.module("movieApp", []);
 
-let app=angular.module("movieApp",[
-    "ngRoute"
-])
-app.config(function($routeProvider,$locationProvider){
-    $routeProvider
-    .when("/",{
-        templateUrl:"sumasree-haritha-Movie-Information-Hub/pages/main.html"
-    })
-    .when("/navBar",{
-        templateUrl:"sumasree-haritha-Movie-Information-Hub/pages/navBar.html"
-    })
+app.controller("MainController", function ($scope, $http) {
+    $scope.movies = [];
+    const apiKey = "9c6f32a";
 
-    $locationProvider.html5Mode({
-        enabled:true,
-        requireBase:false
-
-    })
-})
-
+    $scope.search = function () {
+        if ($scope.searchValue) {
+            $http
+                .get(
+                    `https://www.omdbapi.com/?s=${encodeURIComponent($scope.searchValue)}&apikey=${apiKey}`
+                )
+                .then(function (response) {
+                    if (response.data && response.data.Search) {
+                        $scope.movies = response.data.Search;
+                    } else {
+                        $scope.movies = [];
+                        alert("No movies found for search term");
+                    }
+                })
+                .catch(function (error) {
+                    console.error("Error fetching movies from OMDB:", error);
+                });
+        } else {
+            alert("Please enter a search term.");
+        }
+    };
+});
